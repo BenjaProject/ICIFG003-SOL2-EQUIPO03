6d4a37e17ed8afb49c47a88affcacd108890bb7f
BenjaProject <benjamincamposv2004@gmail.com>
2026-06-16 10:30:49 -0400

    fix: nombre de bff.log a backend.log

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,8 @@
 package com.example.backend;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 		SpringApplication.run(BackendApplication.class, args);
+		logger.info("este es un logger");
 	}
 
 }
